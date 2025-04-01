f36d0df54bb048a4f62588ef712b7f52b6d58881
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-01 10:10:03 +0530

    Connect css file and todo.ejs file with index

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express'); // this line import empress framework 
                                     // reqire() node js function that brings express library
+                                    
 const app = express(); // this is our express application
-
+app.use("/static", express.static("public")); //express.static is a middleware function in express js to help access the static files like images, css from public 
+app.set("view engine", "ejs"); //This specifies the template engine to use, in this case ejs.
 
 //set up a route 
 app.get('/', (req, res) => { // This tells the app to do something when someone visits the root URL ("/")
                              // req ; repersent the request that came from the visitor
                              // res ; repersent the response that will be sent back to the visitor
-    res.send('Hello World!');
+res.render('todo.ejs') 
 });
 
 //start the server 
