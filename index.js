662829ab6853ff8329c6663ba22264430c2d627f
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-01 09:48:57 +0530

    Added comments for more clarification

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // this line import empress framework 
+                                    // reqire() node js function that brings express library
+const app = express(); // this is our express application
 
-app.get('/', (req, res) => {
+
+//set up a route 
+app.get('/', (req, res) => { // This tells the app to do something when someone visits the root URL ("/")
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {console.log('Server is running on port 3000')});
+//start the server 
+app.listen(3000, () => {console.log('Server is running on port 3000')}); //This makes the app listen for visitors on port 3000
+                                                                         //console.log(...): Once the server is running, it prints a message in the terminal to let you know it's working.
